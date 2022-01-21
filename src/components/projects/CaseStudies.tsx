159a27fe1752b4bf5c2ed38553e5a9f5efcabd4d
Vincent P. Dizon <workvpdizon@gmail.com>
2022-01-21 08:09:10 +0800

    removed unused props

--- src/components/projects/CaseStudies.tsx
+++ src/components/projects/CaseStudies.tsx
@@ -2,10 +2,10 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Box, Divider, Pagination } from "@mui/material";
 
-const CaseStudies = (props) => {
+const CaseStudies = () => {
   return (
     <Box>
-     {/*  <Pagination count={2} /> */}
+      {/*  <Pagination count={2} /> */}
       <Divider sx={{ marginY: 2 }} />
     </Box>
   );
