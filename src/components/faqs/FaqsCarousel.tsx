73ca2fd5d88367d923f44c0ded470b1f3cfa957a
Vincent P. Dizon <workvpdizon@gmail.com>
2022-01-26 17:01:45 +0800

    update faqs carousel

--- src/components/faqs/FaqsCarousel.tsx
+++ src/components/faqs/FaqsCarousel.tsx
@@ -3,50 +3,46 @@ import { Box, Card } from "@mui/material";
 
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from "react-responsive-carousel";
+import faqs from "./utils";
 const FaqsCarousel = () => {
   return (
     <Box marginY={5} padding={5} sx={{ marginX: { lg: "10rem" } }}>
-      <Carousel showIndicators={false} showThumbs={false} showArrows={false}>
-        <Box
-          display="flex"
-          flexDirection="column"
-          justifyContent="space-between"
-          padding={2}
-        >
-          <Card
-            raised
-            sx={{
-              width: "fit-content",
-              alignSelf: "flex-start",
-              padding: 3,
-              borderRadius: "10px 0",
-            }}
+      <Carousel autoPlay={true} infiniteLoop showIndicators={false} showThumbs={false} showArrows={false}>
+        {faqs.map(({ id, answer, question }) => (
+          <Box
+            display="flex"
+            flexDirection="column"
+            justifyContent="space-between"
+            padding={2}
+            key={id}
           >
-            <Typography textAlign="left">What can you offer to me?</Typography>
-          </Card>
-          <Card
-            raised
-            sx={{
-              backgroundColor: "#ce7831",
-              color: "white",
-              width: { lg: "30vw" },
-              alignSelf: "flex-end",
-              padding: 3,
-              borderRadius: "10px 0",
-              marginY: 5,
-            }}
-          >
-            <Typography textAlign="left">
-              We can offer you free consultation and free development
-            </Typography>
-          </Card>
-        </Box>
-        <div>
-          <p className="ss">ss 2</p>
-        </div>
-        <div>
-          <p className="ss">Legend 3</p>
-        </div>
+            <Card
+              raised
+              sx={{
+                width: "fit-content",
+                alignSelf: "flex-start",
+                padding: 3,
+                borderRadius: "10px 0",
+              }}
+            >
+              <Typography textAlign="left">{question}</Typography>
+            </Card>
+            <Card
+              raised
+              sx={{
+                backgroundColor: "#ce7831",
+                color: "white",
+                width: { lg: "30vw" },
+                alignSelf: "flex-end",
+                padding: 3,
+                borderRadius: "10px 0",
+                marginY: 5,
+              }}
+            >
+              <Typography textAlign="left">{answer}</Typography>
+            </Card>
+          </Box>
+        ))}
       </Carousel>
     </Box>
   );
---
+++ src/components/faqs/utils/index.ts
@@ -0,0 +1,19 @@
+const faqs = [
+  {
+    id: 1,
+    question: "What can you offer to me?",
+    answer: "We can offer you free consultation and free development",
+  },
+  {
+    id: 2,
+    question: "What type of websites can you do?",
+    answer: "We can do a full-stack website for you.",
+  },
+  {
+    id: 3,
+    question: "Does your free website development expire?",
+    answer: "No. It is free forever.",
+  },
+];
+
+export default faqs;
